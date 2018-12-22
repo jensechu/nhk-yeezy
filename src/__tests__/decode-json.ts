f350b88355bb4777d1e369d03a0a9f42695cfa57
Jensen <jensen@cloudflare.com>
2018-12-22 16:11:15 -0600

    Simple database for articles with fake creds

--- src/__tests__/decode-json.ts
+++
@@ -1,7 +0,0 @@
-import * as encodedJSON from "./fixtures/encoded-json.json";
-import * as decodedJSON from "./fixtures/decoded-json.json";
-import decodeJSON from "../decode-json";
-
-test("decodeJSON returns decoded JSON", () => {
-  expect(decodeJSON(JSON.stringify(encodedJSON))).toEqual(decodedJSON.default);
-});
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
-import decodeJSON from "./decode-json";
+import decodeJSON from "./utilities/decode-json";
+import { createConnection, getConnection } from "typeorm";
+
+createConnection();
 
 axios
   .get("http://www3.nhk.or.jp/news/easy/top-list.json")
   .then(function(response: any) {
     const allArticles = decodeJSON(response.data);
+
+    for (let article in allArticles) {
+      console.log(getConnection());
+    }
   })
   .catch(function(error: any) {
     console.log(error);
