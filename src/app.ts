903af8cc82407482d56b3c3ebad7a2fb5f479e79
Jensen <jensen@cloudflare.com>
2018-12-22 21:21:43 -0600

    Article controller to ad one or multiple articles

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,16 @@
 import axios from "axios";
 import decodeJSON from "./utilities/decode-json";
-import { createConnection, getConnection, getRepository } from "typeorm";
+import { insertNewArticles } from "./article-controller";
+import { createConnection } from "typeorm";
+
+const URL = "http://www3.nhk.or.jp/news/easy/top-list.json";
 
 createConnection();
 
 axios
-  .get("http://www3.nhk.or.jp/news/easy/top-list.json")
+  .get(URL)
   .then(function(response: any) {
-    const allArticles = decodeJSON(response.data);
-
-    allArticles.forEach((article: any) => {
-      getConnection()
-        .getRepository("Article")
-        .findOne({ news_id: `${article.news_id}` })
-        .then((hasArticle: any) => {
-          if (!hasArticle)
-            getConnection()
-              .createQueryBuilder()
-              .insert()
-              .into("Article")
-              .values(article)
-              .execute();
-        });
-    });
+    insertNewArticles(decodeJSON(response.data));
   })
   .catch(function(error: any) {
     console.log(error);
